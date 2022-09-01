1d0b5f603d4e1a09e7fd0fc0c82fee864164512c
Olga <olyakapran@yandex.by>
2022-09-01 14:38:33 +0300

    first commit

---
+++ задача25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace задача25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Stepen()
+            {
+            Console.Write("Введите первое число ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{a}^{b} = " + Math.Pow(a, b)); 
+            }
+            Stepen();
+        }
+    }
+}

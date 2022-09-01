b48b1188fd305b78208358e78e52076bd99e20b5
Olga <olyakapran@yandex.by>
2022-09-01 14:39:55 +0300

    second commit

---
+++ задача27/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace задача27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int x=Convert.ToInt32(Console.ReadLine());
+            int n=x;
+            int sum = 0;
+            while (x > 0)
+            {
+                sum = sum + x % 10;
+                x = x /10 ;
+            }
+            Console.WriteLine($"Cумма цифр в числе {n} = " + sum);
+            
+
+        }
+    }
+}
